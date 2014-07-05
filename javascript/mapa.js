f0297981a422c9308537aa721ecc6b16533bac01
Daniela Aguirre <daniela@codeismo.com>
2014-07-05 09:12:05 -0500

    cambios clase 5

--- javascript/mapa.js
+++ javascript/mapa.js
@@ -1,8 +1,17 @@
 $(function() {
-	navigator.geolocation.getCurrentPosition(function(posicion){
+	navigator.geolocation.getCurrentPosition(function(posicion) {
 		var latitud = posicion.coords.latitude;
 		var longitud = posicion.coords.longitude;
-		alert("lon " + longitud + " lat " + latitud);
+		//alert("lon " + longitud + " lat " + latitud);
+		//pintar el mapa
+		$("#mapa").goMap({
+			markers : [{
+				latitude: latitud,
+				longitude: longitud
+			}],
+			zoom : 17
+		});
+
 	});
 
 });
