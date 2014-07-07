199aa45867873cd5d6e11912c10c2438665b304d
Daniela Aguirre <daniela@codeismo.com>
2014-07-07 01:58:25 -0500

    atender peticiones GET y POST mediante AJAX

--- servidor.js
+++ servidor.js
@@ -14,81 +14,116 @@ servidor.listen(8080);
 console.log("servidor levantado");
 
 /*usamos body parser para recibir datos del cliente*/
-app.use( bodyParser.urlencoded({
-	extended: true
-}) );
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
 console.log("body parser configurado");
 
-/*configurar vistas estaticas css, videos, imagenes, 
+/*configurar vistas estaticas css, videos, imagenes,
  * fuentes, javascript*/
-app.use("/videos", express.static( __dirname + "/videos" ) );
-app.use("/fuentes", express.static( __dirname + "/fuentes" ) );
-app.use("/imagenes", express.static( __dirname + "/imagenes" ) );
-app.use("/css", express.static( __dirname + "/css" ) );
-app.use("/javascript", express.static( __dirname + "/javascript" ) );
+app.use("/videos", express.static(__dirname + "/videos"));
+app.use("/fuentes", express.static(__dirname + "/fuentes"));
+app.use("/imagenes", express.static(__dirname + "/imagenes"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/javascript", express.static(__dirname + "/javascript"));
 
 console.log("rutas estaticas configuradas");
 
 /*configurar la carpeta de vistas*/
-nunjucks.configure( __dirname + "/vistas", {
-	express: app
-} );
+nunjucks.configure(__dirname + "/vistas", {
+	express : app
+});
 
 console.log("sistemas de templates configurado");
-app.get("/", function(req, res){
+
+app.get("/", function(req, res) {
 	//res.send("Repondiendo a la peticion get /");
 	res.render("index.html");
-} );
+});
 
-app.get("/galeria", function(req, res){
-	//res.send("Repondiendo a la peticion get /");
-	res.render("galeria.html");
-} );
-
-app.get("/ubicacion", function(req, res){
-	//res.send("Repondiendo a la peticion get /");
-	res.render("ubicacion.html");
-} );
-
-app.get("/chat", function(req, res){
-	//res.send("Repondiendo a la peticion get /");
-	res.render("chat.html");
-} );
-
-app.get("/contacto", function(req, res){
-	//res.send("Repondiendo a la peticion get /");
-	res.render("contacto.html");
-} );
-
-app.get("/index_contenido", function(req, res){
-	//res.send("Repondiendo a la peticion get /");
+app.get("/index_contenido", function(req, res) {
+	//res.send("Repondiendo a la peticion get /index_contenido");
 	res.render("index_contenido.html");
-} );
+});
 
-app.get("/galeria_contenido", function(req, res){
-	//res.send("Repondiendo a la peticion get /");
+app.get("/galeria", function(req, res) {
+	//res.send("Repondiendo a la peticion get /galeria");
+	res.render("galeria.html");
+});
+
+app.get("/galeria_contenido", function(req, res) {
+	//res.send("Repondiendo a la peticion get /galeria_contenido");
 	res.render("galeria_contenido.html");
-} );
+});
+
+app.get("/ubicacion", function(req, res) {
+	//res.send("Repondiendo a la peticion get /ubicacion");
+	res.render("ubicacion.html");
+});
+
+app.get("/ubicacion_contenido", function(req, res) {
+	//res.send("Repondiendo a la peticion get /ubicacion_contenido");
+	res.render("ubicacion_contenido.html");
+});
+
+app.get("/chat", function(req, res) {
+	//res.send("Repondiendo a la peticion get /chat");
+	res.render("chat.html");
+});
+
+app.get("/chat_contenido", function(req, res) {
+	//res.send("Repondiendo a la peticion get /chat_contenido");
+	res.render("chat_contenido.html");
+});
+
+app.get("/contacto", function(req, res) {
+	//res.send("Repondiendo a la peticion get /contacto");
+	res.render("contacto.html");
+});
+
+app.get("/contacto_contenido", function(req, res) {
+	//res.send("Repondiendo a la peticion get /contacto_contenido");
+	res.render("contacto_contenido.html");
+});
 
 /*respoder a una peticion post*/
-app.post("/suscribir", function(req, res){
+app.post("/suscribir", function(req, res) {
 	var parametroEmail = req.body.correo;
-	console.log("Recibi " + parametroEmail );
-	
-	res.render("suscrito.html",{
-		email: parametroEmail
-	});
-} );
+	console.log("Recibi " + parametroEmail);
+	//por default se responde con suscribir.html
+	var respuesta = "suscribir.html";
+	//pero si la peticion es ajax se responde con suscribir_contenido.html
+	if (req.body.esAjax) {
+		respuesta = "suscribir_contenido.html";
+	}
 
-app.post("/contactar", function(request, response){
+	res.render(respuesta, {
+		email : parametroEmail
+	});
+});
+
+app.post("/contactar", function(request, response) {
 	var nombre = request.body.nombre;
 	var email = request.body.correo;
 	var web = request.body.sitio;
 	var comentario = request.body.comentario;
-	response.render("contactar.html",{
-		nombre: nombre,
-		emai: email,
-		comentario: comentario
+
+	console.log(nombre);
+	console.log(email);
+	console.log(web);
+	console.log(comentario);
+
+	//por default se responde con contactar.html
+	var respuesta = "contactar.html";
+	//pero si la peticion es ajax se responde con contactar_contenido.html
+	if (request.body.esAjax) {
+		respuesta = "contactar_contenido.html";
+	}
+
+	response.render(respuesta, {
+		nombre : nombre,
+		email : email,
+		comentario : comentario
 	});
 });
 
@@ -96,22 +131,22 @@ app.post("/contactar", function(request, response){
 var io = socketio.listen(servidor);
 //escuchar mensaje de cualqueir cliente
 var contador = 0;
-io.sockets.on("connection", function(socket){
+io.sockets.on("connection", function(socket) {
 	//actualizar contador
 	contador = contador + 1;
 	//enviar un mensaje a los clientes
-	io.sockets.emit("actualizar_contador",{
-		clientes: contador
+	io.sockets.emit("actualizar_contador", {
+		clientes : contador
 	});
-	
+
 	//enviarle el mensaje que recibi a todos los demas clientes
-	socket.on("mensaje_al_servidor", function(datos){
+	socket.on("mensaje_al_servidor", function(datos) {
 		console.log(datos.nombre);
 		console.log(datos.mensaje);
 		//enviale mensaje a todos los clientes
-		io.sockets.emit("mensaje_al_cliente",{
-			mensaje: sanitizer.escape(datos.mensaje),
-			nombre: sanitizer.escape(datos.nombre)
+		io.sockets.emit("mensaje_al_cliente", {
+			mensaje : sanitizer.escape(datos.mensaje),
+			nombre : sanitizer.escape(datos.nombre)
 		});
 	});
 });
